194d0a491932ec2679e33eb294f80c3c34c04ad6
pascalpradel <pascaloepitz@web.de>
2021-07-11 17:13:09 +0200

    added oneshot.py

---
+++ OneShot.py
@@ -0,0 +1,34 @@
+import keyboard
+import mouse
+import time
+
+class Macro(object):
+    def __init__(self):
+        print("Init started..")
+
+    def start(self):
+        time.sleep(5)
+        print("Started..")
+        key = False
+        isPressed = False
+
+        while True:
+            if mouse.is_pressed('left') == False:
+                if isPressed == True:
+                    isPressed = False
+
+            if mouse.is_pressed('left') == True:
+                if isPressed == False:
+                    key = True
+                    isPressed = True
+
+            time.sleep(0.02)
+
+            if key:
+                keyboard.press_and_release('la')
+                print("Pressed")
+                key = False
+            
+if __name__ == "__main__":
+    macro = Macro()
+    macro.start()
